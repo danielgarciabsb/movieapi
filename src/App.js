f9c5251b0bc83de1855c12e9e08bede3eecff81b
daniel.garcia <daniel.garcia@datameaning.com>
2020-07-19 23:10:16 -0300

    Genres
    Genres
    
    
    Genres
    
    
    Genres

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import api from './api';
 import trending from './trending';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import Filme from './Filme'
+import FilmeGenre from './FilmeGenre'
+import genres from "./genres";
 
 class App extends Component {
 
   state = {
     filmes: [],
     trending: [],
+    genres: [],
+    withgenres: new Map(),
     txt_busca: 'titanic'
   };
 
@@ -24,9 +28,12 @@ class App extends Component {
   async componentDidMount() {
     this.response = await api.get(this.state.txt_busca);
     this.trending = await trending.get();
+    this.genres = await genres.get();
+
+    this.setState({ genres: this.genres.data.genres });
     this.setState({ filmes: this.response.data.results });
     this.setState({ trending: this.trending.data.results });
-    console.log(this.state.filmes);
+
   }
 
   handleChange(event) {
@@ -51,14 +58,18 @@ class App extends Component {
     return (
         <Container className="p-3">
           <Jumbotron>
-            <h1 className="header">Trending</h1>
-            <Filme listaFilmes={this.state.trending} />
-          </Jumbotron>
-          <Jumbotron>
             <h1 className="header">Filmes por título</h1>
             <input type="text" value={this.state.txt_busca} onChange={this.handleChange} onKeyDown={this.handleKeyDown}/>
             <Button variant="primary" onClick={this.handleClick}>Buscar</Button>
             <Filme listaFilmes={this.state.filmes} />
+          </Jumbotron>
+          <Jumbotron>
+            <h1 className="header">Trending</h1>
+            <Filme listaFilmes={this.state.trending} />
+          </Jumbotron>
+          <Jumbotron>
+            <h1 className="header">Popular</h1>
+            <FilmeGenre genres={this.state.genres} />
           </Jumbotron>
         </Container>
 
